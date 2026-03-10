61c3202abb2c081e1265d64f366e6a56bcfda7b2
Dauren <d.k.nurgaziev@gmail.com>
2026-03-10 17:48:31 +0500

    succesful data migration

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=50, null=True, blank=True)
+    first_name = models.CharField(max_length=50)
