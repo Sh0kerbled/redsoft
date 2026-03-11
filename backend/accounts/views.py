256fbd809bb919988d9fec4cda8bec1bd7efa802
Dauren <d.k.nurgaziev@gmail.com>
2026-03-11 16:32:23 +0500

    login by using django templates

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def page(request):
     return render(request, 'accounts/pages.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from accounts.views import page
+from django.urls import path, include
+from accounts.views import page, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/', page)
+    path('pages/', page),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', register)
 ]
