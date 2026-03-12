afd516229811fd66390ea153cf455ca313bc1e25
Dauren <d.k.nurgaziev@gmail.com>
2026-03-12 11:22:43 +0500

    simply lovely

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,12 +4,7 @@ from accounts.views import api_register, api_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # СНАЧАЛА ставим ваши кастомные API-пути
     path('accounts/register/', api_register, name='register'),
-    path('accounts/login/', api_login, name='login'), # Исправлено: добавлено 'accounts/'
-    
-    # ПОТОМ подключаем встроенные пути Django (если они вообще вам нужны)
-    # Django проверит верхние пути первыми, и ваш 'accounts/login/' сработает вместо дефолтного
+    path('accounts/login/', api_login, name='login'),
     path('accounts/', include('django.contrib.auth.urls')), 
 ]
